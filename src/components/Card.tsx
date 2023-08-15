59f2702f0aa0b27e8e539e538260a78a00ebce3b
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-15 15:43:36 +0100

    Getting there

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,13 +3,16 @@ import "../App.css";
 import CardBody from "./CardBody";
 
 import SignUpMobile from "../assets/images/illustration-sign-up-mobile.svg";
+import SignUpDesktop from "../assets/images/illustration-sign-up-desktop.svg";
 
 const Card = () => {
   return (
     <>
-      <div className="flex flex-col bg-whites font-roboto ">
-        <img className="" src={SignUpMobile} alt="A sign-up picture" />
-        <CardBody />
+      <div className="flex flex-col md:bg-charcoal-greys font-roboto  md:justify-center md:items-center md:h-screen">
+        <div className="md:bg-whites md:flex-row-reverse flex flex-col">
+          <img className="" src={SignUpMobile} alt="A sign-up picture" />
+          <CardBody />
+        </div>
       </div>
     </>
   );
