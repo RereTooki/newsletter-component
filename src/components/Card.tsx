cf8d1f34bfe8770c5c118f3a044979f037bd25ea
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-15 22:55:40 +0100

    Dealing with layout issues

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,13 +9,13 @@ const Card = () => {
   return (
     <>
       <div className=" h-fit w-full flex flex-col md:bg-charcoal-greys font-roboto  md:justify-center md:items-center md:h-max md:py-[55px]">
-        <div className=" md:w-fit md:h-screen object-cover md:bg-whites md:justify-center md:items-center md:flex-row-reverse flex flex-col rounded-[25px] ">
+        <div className=" md:w-fit md:h-fit object-cover md:bg-whites md:justify-center md:items-center md:flex-row-reverse flex flex-col rounded-[25px] ">
           <img
-            className="md:w-1/2 relative top-[-20px] md:top-0  md:py-[15px] md:w-fit md:h-full md:px-[25px]  "
+            className="md:w-1/2 md:h-1/2 relative top-[-20px] md:top-0  md:py-[15px] md:w-fit  md:px-[25px]  "
             src={SignUpDesktop}
             alt="A sign-up picture"
           />
-          <div className="md:w-1/2 md:justify-center md:items-center">
+          <div className="md:w-1/2 md:h-1/2 md:justify-center md:items-center">
             <CardBody />
           </div>
         </div>
