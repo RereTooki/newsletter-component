9a618ecf2046d7ccf83fdb2e87bb815d2ae04256
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-16 12:54:35 +0100

    Stored the data collected in an obj. Unnecessary.

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -3,10 +3,12 @@ import "../App.css";
 
 const CardForm = () => {
   const emailRef = useRef<HTMLInputElement>(null);
+  const person = { email: "" };
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (emailRef.current !== null) console.log(emailRef.current.value);
+    if (emailRef.current !== null) person.email = emailRef.current.value;
+    console.log(person);
   };
   return (
     <>
