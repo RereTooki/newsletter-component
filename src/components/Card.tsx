d69d5b496084d9866052976a8dfee7c21b9fa9a2
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-16 23:35:47 +0100

    Showedthe first page 6 times to check layout  iss.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,35 +11,182 @@ const Card = () => {
   return (
     <>
       {isVisible && (
-        <div className=" h-full w-full flex flex-col md:bg-charcoal-greys font-roboto  md:justify-center md:items-center md:h-max md:py-[55px]">
-          <div className=" md:w-fit md:h-fit object-cover md:bg-whites md:justify-center md:items-center md:flex-row-reverse flex flex-col rounded-[25px] ">
-            <img
-              className="md:h-1/2  md:w-1/2  relative top-[-20px] md:top-0  md:py-[15px] md:px-[25px]  "
-              src={SignUpDesktop}
-              alt="A sign-up picture"
-            />
-            <div className=" px-[15px] md:w-1/2 md:h-1/2 md:justify-center md:items-center">
-              <CardBody onClick={() => setIsVisible(false)} />
+        <>
+          <div className=" h-full w-full flex flex-col md:bg-charcoal-greys font-roboto  md:justify-center md:items-center md:h-max md:py-[55px]">
+            <div className=" md:w-fit md:h-fit object-cover md:bg-whites md:justify-center md:items-center md:flex-row-reverse flex flex-col rounded-[25px] ">
+              <img
+                className="md:h-1/2  md:w-1/2  relative top-[-20px] md:top-0  md:py-[15px] md:px-[25px]  "
+                src={SignUpDesktop}
+                alt="A sign-up picture"
+              />
+              <div className=" px-[15px] md:w-1/2 md:h-1/2 md:justify-center md:items-center">
+                <CardBody onClick={() => setIsVisible(false)} />
+              </div>
+            </div>
+            <div className="attribution">
+              Challenge by{" "}
+              <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a
+                href="https://linkedin.com/in/rerel-oluwa-tooki-b53396253/"
+                target="_blank"
+              >
+                Rere Tooki
+              </a>
+              .
             </div>
           </div>
-          <div className="attribution">
-            Challenge by{" "}
-            <a
-              href="https://www.frontendmentor.io?ref=challenge"
-              target="_blank"
-            >
-              Frontend Mentor
-            </a>
-            . Coded by{" "}
-            <a
-              href="https://linkedin.com/in/rerel-oluwa-tooki-b53396253/"
-              target="_blank"
-            >
-              Rere Tooki
-            </a>
-            .
+          <div className=" h-full w-full flex flex-col md:bg-charcoal-greys font-roboto  md:justify-center md:items-center md:h-max md:py-[55px]">
+            <div className=" md:w-fit md:h-fit object-cover md:bg-whites md:justify-center md:items-center md:flex-row-reverse flex flex-col rounded-[25px] ">
+              <img
+                className="md:h-1/2  md:w-1/2  relative top-[-20px] md:top-0  md:py-[15px] md:px-[25px]  "
+                src={SignUpDesktop}
+                alt="A sign-up picture"
+              />
+              <div className=" px-[15px] md:w-1/2 md:h-1/2 md:justify-center md:items-center">
+                <CardBody onClick={() => setIsVisible(false)} />
+              </div>
+            </div>
+            <div className="attribution">
+              Challenge by{" "}
+              <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a
+                href="https://linkedin.com/in/rerel-oluwa-tooki-b53396253/"
+                target="_blank"
+              >
+                Rere Tooki
+              </a>
+              .
+            </div>
           </div>
-        </div>
+          <div className=" h-full w-full flex flex-col md:bg-charcoal-greys font-roboto  md:justify-center md:items-center md:h-max md:py-[55px]">
+            <div className=" md:w-fit md:h-fit object-cover md:bg-whites md:justify-center md:items-center md:flex-row-reverse flex flex-col rounded-[25px] ">
+              <img
+                className="md:h-1/2  md:w-1/2  relative top-[-20px] md:top-0  md:py-[15px] md:px-[25px]  "
+                src={SignUpDesktop}
+                alt="A sign-up picture"
+              />
+              <div className=" px-[15px] md:w-1/2 md:h-1/2 md:justify-center md:items-center">
+                <CardBody onClick={() => setIsVisible(false)} />
+              </div>
+            </div>
+            <div className="attribution">
+              Challenge by{" "}
+              <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a
+                href="https://linkedin.com/in/rerel-oluwa-tooki-b53396253/"
+                target="_blank"
+              >
+                Rere Tooki
+              </a>
+              .
+            </div>
+          </div>
+          <div className=" h-full w-full flex flex-col md:bg-charcoal-greys font-roboto  md:justify-center md:items-center md:h-max md:py-[55px]">
+            <div className=" md:w-fit md:h-fit object-cover md:bg-whites md:justify-center md:items-center md:flex-row-reverse flex flex-col rounded-[25px] ">
+              <img
+                className="md:h-1/2  md:w-1/2  relative top-[-20px] md:top-0  md:py-[15px] md:px-[25px]  "
+                src={SignUpDesktop}
+                alt="A sign-up picture"
+              />
+              <div className=" px-[15px] md:w-1/2 md:h-1/2 md:justify-center md:items-center">
+                <CardBody onClick={() => setIsVisible(false)} />
+              </div>
+            </div>
+            <div className="attribution">
+              Challenge by{" "}
+              <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a
+                href="https://linkedin.com/in/rerel-oluwa-tooki-b53396253/"
+                target="_blank"
+              >
+                Rere Tooki
+              </a>
+              .
+            </div>
+          </div>
+          <div className=" h-full w-full flex flex-col md:bg-charcoal-greys font-roboto  md:justify-center md:items-center md:h-max md:py-[55px]">
+            <div className=" md:w-fit md:h-fit object-cover md:bg-whites md:justify-center md:items-center md:flex-row-reverse flex flex-col rounded-[25px] ">
+              <img
+                className="md:h-1/2  md:w-1/2  relative top-[-20px] md:top-0  md:py-[15px] md:px-[25px]  "
+                src={SignUpDesktop}
+                alt="A sign-up picture"
+              />
+              <div className=" px-[15px] md:w-1/2 md:h-1/2 md:justify-center md:items-center">
+                <CardBody onClick={() => setIsVisible(false)} />
+              </div>
+            </div>
+            <div className="attribution">
+              Challenge by{" "}
+              <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a
+                href="https://linkedin.com/in/rerel-oluwa-tooki-b53396253/"
+                target="_blank"
+              >
+                Rere Tooki
+              </a>
+              .
+            </div>
+          </div>
+          <div className=" h-full w-full flex flex-col md:bg-charcoal-greys font-roboto  md:justify-center md:items-center md:h-max md:py-[55px]">
+            <div className=" md:w-fit md:h-fit object-cover md:bg-whites md:justify-center md:items-center md:flex-row-reverse flex flex-col rounded-[25px] ">
+              <img
+                className="md:h-1/2  md:w-1/2  relative top-[-20px] md:top-0  md:py-[15px] md:px-[25px]  "
+                src={SignUpDesktop}
+                alt="A sign-up picture"
+              />
+              <div className=" px-[15px] md:w-1/2 md:h-1/2 md:justify-center md:items-center">
+                <CardBody onClick={() => setIsVisible(false)} />
+              </div>
+            </div>
+            <div className="attribution">
+              Challenge by{" "}
+              <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a
+                href="https://linkedin.com/in/rerel-oluwa-tooki-b53396253/"
+                target="_blank"
+              >
+                Rere Tooki
+              </a>
+              .
+            </div>
+          </div>
+        </>
       )}
       {isVisible || (
         <>
