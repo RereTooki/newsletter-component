d893c88fa2701bdfb1507619e1da1424dfa8b05a
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-17 07:40:54 +0100

    Trying to fix the mobile layout issue

--- src/components/CardBody2.tsx
+++ src/components/CardBody2.tsx
@@ -5,12 +5,12 @@ import IconSuccess from "../assets/images/icon-success.svg";
 const CardBody2 = () => {
   return (
     <>
-      <div className="md:whitespace-normal">
+      <div className="md:whitespace-nowrap">
         <img className="" src={IconSuccess} alt="A success icon" />
-        <h1 className="leading-[45px] text-dark-slate-greys text-[44px] font-bold mt-[30px] mb-[20px]">
+        <h1 className="leading-[45px] text-dark-slate-greys text-[44px] font-bold mt-[30px] mb-[20px] md:mt-[10px] md:mb-[5px] md:text-[24px] md:leading-[25px]">
           Thanks for subscribing!
         </h1>
-        <p className="text-charcoal-greys">
+        <p className="text-charcoal-greys md:text-[14px] md:whitespace-normal">
           A confirmation email has been sent to <b>rere.</b> Please open it and
           click the button inside to confirm your subscription.
         </p>
